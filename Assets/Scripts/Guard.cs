ae19d3e8782b9a8865e1029b1fe3f4574078cf21
hanneysaurus <hanneysaurus@hotmail.com>
2019-11-15 12:24:08 +0100

    added improved guard script

--- Assets/Scripts/Guard.cs
+++ Assets/Scripts/Guard.cs
@@ -7,13 +7,32 @@ using UnityEngine.SceneManagement;
 public class Guard : MonoBehaviour
 {
     public Collider2D player_collider;
-    private GameObject playerObject;
+    public Transform waypointsObject;
+    public GameObject roomLights;
+    public float speed;
     public string next_scene;
+    public bool detected;
+    private GameObject playerObject;
+    
+    private GameObject target;
+    private GameObject[] waypoints;
+    private int waypointIndex;
+    
 
     // Start is called before the first frame update
     void Start()
     {
+        waypointIndex = 0;
         playerObject = player_collider.gameObject;
+        detected = false;
+        target = playerObject;
+
+        int waypointAmount = waypointsObject.childCount;
+        waypoints = new GameObject[waypointAmount];
+        for (int i = 0; i < waypointAmount; i++)
+        {
+            waypoints[i] = waypointsObject.GetChild(i).gameObject;
+        }
     }
 
     /*
@@ -29,11 +48,37 @@ public class Guard : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float deltaY = transform.position.y - playerObject.transform.position.y;
-        float deltaX = transform.position.x - playerObject.transform.position.x;
+        if (detected)
+        {
+            target = playerObject;
+            speed = 0.05f;
+        }
+        else
+            target = waypoints[waypointIndex];
 
-        float posX = transform.position.x - 0.01f * (float)Math.Cos(Math.Atan2(deltaY, deltaX));
-        float posY = transform.position.y - 0.01f * (float)Math.Sin(Math.Atan2(deltaY, deltaX));
+        
+        float deltaX = transform.position.x - target.transform.position.x;
+        float deltaY = transform.position.y - target.transform.position.y;
+        float angleX = (float)Math.Cos(Math.Atan2(deltaY, deltaX));
+        float angleY = (float)Math.Sin(Math.Atan2(deltaY, deltaX));
+
+        if (target == waypoints[waypointIndex] && Math.Abs(deltaY) < 1 && Math.Abs(deltaX) < 1)
+        {
+            waypointIndex ++;
+            if (waypointIndex == waypoints.Length)
+                waypointIndex = 0;
+            target = waypoints[waypointIndex];
+        } 
+        else if (!detected && roomLights.activeSelf) 
+        {
+            LayerMask playerLayer = LayerMask.GetMask("Collidables");
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(-angleX, -angleY), 10, playerLayer);
+            if (hit.transform != null && hit.transform.name == "Player Object (Terry the thief)")
+                detected = true;
+        }
+        
+        float posX = transform.position.x - speed * angleX;
+        float posY = transform.position.y - speed * angleY;
         transform.position = new Vector3(posX, posY, 0);
     }
 }
