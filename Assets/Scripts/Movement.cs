f56cba82ce31952dfb79a84cb9d82e44d3ccc074
hanneysaurus <hanneysaurus@hotmail.com>
2019-12-09 04:44:12 +0100

    added level 'met'

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -153,13 +153,7 @@ public class Movement : MonoBehaviour
     }
 
     void Animate() {
-
-        //animator;
-        Debug.Log(velocity);
-        Debug.Log(Mathf.Abs(this.velocity.x) < Mathf.Abs(this.velocity.y));
-        Debug.Log(this.velocity.y > 0);
-        Debug.Log(this.velocity.x > 0);
-
+        
         if (Mathf.Abs(this.velocity.x) < 0.01f && Mathf.Abs(this.velocity.y) < 0.01f)
         {
             animator.SetBool("downwards", false);
@@ -172,6 +166,7 @@ public class Movement : MonoBehaviour
         {
             animator.enabled = true;
         }
+
         if (Mathf.Abs(this.velocity.x) < Mathf.Abs(this.velocity.y))
         {
             if (this.velocity.y > 0)
